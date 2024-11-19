75a8a8477d423810f1ee2d85ba1bb978f45979d2
Abelardo Aldana Angulo <L23170066@culiacan.tecnm.mx>
2024-11-19 16:04:02 +0000

    FUGA

---
+++ arboles/ArbolBinarioBusqueda.java
@@ -0,0 +1,87 @@
+package arboles;
+
+public class ArbolBinarioBusqueda extends ArbolBinario {
+
+    public ArbolBinarioBusqueda() {
+        super();
+    }
+
+    public ArbolBinarioBusqueda (Nodo raiz) {
+        super(raiz);
+    }
+
+    // Inserta un nodo en el arbol binario
+    public void insertar(Object valor) throws Exception {
+
+        Comparable dato = (Comparable) valor;
+
+        Nodo nuevo = new Nodo();
+        nuevo.setValor(dato);
+
+        if (raiz == null)
+            raiz = nuevo;
+        else {
+
+            // anterior: Este nodo anterior es una referencia al padre de AUX
+            Nodo anterior = null;
+
+            // Aux = auxiliar que va recorriendo los nodos, desde la raiz 
+            Nodo aux = raiz;
+
+            while (aux != null) {
+                anterior = aux;
+                if (dato.esMenor(aux.getValor()))
+                    aux = aux.getIzquierdo();
+                else if (dato.esMayor(aux.getValor()))
+                    aux = aux.getDerecho();
+                else
+                    throw new Exception("Dato duplicado.");
+            }
+
+            if (dato.esMenor(anterior.getValor()))
+                anterior.setIzquierdo(nuevo);
+            else
+                anterior.setDerecho(nuevo);
+        }
+
+    }
+
+    /*
+     *  Insertar interfaz de la operacion, llama al metodo insertarRecursivo que realiza la operacio
+     *  y de vuelve la raiz del nuevo arbol. A este metodo interno se le pasa la raiz actual, a partir de la cual
+     *  se describe el camino de busquda, y al final se enlaza
+     */
+
+    public void insertar2 (Object valor) throws Exception {
+
+        Comparable dato = (Comparable) valor;
+        raiz = insertarRec (raiz,dato);
+
+    }
+
+    private Nodo insertarRec(Nodo raizSub, Comparable dato) throws Exception {
+        if (raizSub == null) {
+
+            // Caso base, termina la recursividad
+            raizSub = new Nodo(dato);
+        } else {
+            if (dato.esMenor(raizSub.getValor())) {
+                
+                Nodo iz = insertarRec(raizSub.getIzquierdo(), dato);
+                raizSub.setIzquierdo(iz);
+
+            } else if (dato.esMayor(raizSub.getValor())) {
+
+                Nodo dr = insertarRec(raizSub.getDerecho(), dato);
+                raizSub.setDerecho(dr);
+
+            } else {
+                throw new Exception("Dato esta duplicado.");
+            }
+        }
+
+        return raizSub;
+    }
+
+
+}
