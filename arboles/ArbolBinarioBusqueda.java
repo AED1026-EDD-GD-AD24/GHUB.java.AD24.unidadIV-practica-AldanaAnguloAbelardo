6985909489ddff092469be718d4dbdcaf35807c6
Abelardo Aldana Angulo <L23170066@culiacan.tecnm.mx>
2024-11-20 15:59:55 +0000

    buen

--- arboles/ArbolBinarioBusqueda.java
+++ arboles/ArbolBinarioBusqueda.java
@@ -6,7 +6,7 @@ public class ArbolBinarioBusqueda extends ArbolBinario {
         super();
     }
 
-    public ArbolBinarioBusqueda (Nodo raiz) {
+    public ArbolBinarioBusqueda(Nodo raiz) {
         super(raiz);
     }
 
@@ -25,7 +25,7 @@ public class ArbolBinarioBusqueda extends ArbolBinario {
             // anterior: Este nodo anterior es una referencia al padre de AUX
             Nodo anterior = null;
 
-            // Aux = auxiliar que va recorriendo los nodos, desde la raiz 
+            // Aux = auxiliar que va recorriendo los nodos, desde la raiz
             Nodo aux = raiz;
 
             while (aux != null) {
@@ -47,15 +47,17 @@ public class ArbolBinarioBusqueda extends ArbolBinario {
     }
 
     /*
-     *  Insertar interfaz de la operacion, llama al metodo insertarRecursivo que realiza la operacio
-     *  y de vuelve la raiz del nuevo arbol. A este metodo interno se le pasa la raiz actual, a partir de la cual
-     *  se describe el camino de busquda, y al final se enlaza
+     * Insertar interfaz de la operacion, llama al metodo insertarRecursivo que
+     * realiza la operacio
+     * y de vuelve la raiz del nuevo arbol. A este metodo interno se le pasa la raiz
+     * actual, a partir de la cual
+     * se describe el camino de busquda, y al final se enlaza
      */
 
-    public void insertar2 (Object valor) throws Exception {
+    public void insertar2(Object valor) throws Exception {
 
         Comparable dato = (Comparable) valor;
-        raiz = insertarRec (raiz,dato);
+        raiz = insertarRec(raiz, dato);
 
     }
 
@@ -66,7 +68,7 @@ public class ArbolBinarioBusqueda extends ArbolBinario {
             raizSub = new Nodo(dato);
         } else {
             if (dato.esMenor(raizSub.getValor())) {
-                
+
                 Nodo iz = insertarRec(raizSub.getIzquierdo(), dato);
                 raizSub.setIzquierdo(iz);
 
@@ -83,5 +85,65 @@ public class ArbolBinarioBusqueda extends ArbolBinario {
         return raizSub;
     }
 
+    /*
+     * 
+     *  @param, valor a buscar
+     *  @return el nodo encontrado
+     */
 
+    public Nodo buscar(Object valor) {
+        Comparable dato = (Comparable) valor;
+
+        if (raiz == null)
+            return raiz;
+        else {
+            // Aux es un auxiliar que va recorriendo el arbol desde la raiz
+            Nodo aux = raiz;
+            while (aux != null) {
+                if (dato.esIgual(aux.getValor()))
+                    return aux;
+                else if (dato.esMenor(aux.getValor()))
+                    aux = aux.getIzquierdo();
+                else
+                    aux = aux.getDerecho();
+            }
+        }
+        return null;
+    }
+
+    /*
+     * 
+     * Interfaz de buscar que invoca al metodo recursivo localizar
+     *  @param buscado, el valor buscado
+     *  @return Nodo buscado o null si no lo encuentra
+     */
+    public Nodo buscar2 (Object buscado) {
+        Comparable dato = (Comparable) buscado;
+
+        if (raiz == null)
+            return null;
+        else
+            return localizar(raiz, dato);
+    }
+
+
+    /*
+     *  Version recursiva
+     *  @param raizSUb
+     *  @param buscado
+     *  @return Nodo buscado si lo encuentra, si no returna null
+     */
+    
+    private Nodo localizar (Nodo raizSub, Comparable buscado) {
+
+        if (raizSub == null)
+            return null;
+        else if (buscado.esIgual(raizSub.getValor())) {
+            return raizSub;
+        } else if (buscado.esMenor(raizSub.getValor()))
+            return localizar(raizSub.getIzquierdo(), buscado);
+        else 
+            return localizar(raizSub.getDerecho(), buscado);
+
+    }
 }
